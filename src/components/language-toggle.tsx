49f6ad5fba160e44f7a68eb2c18ab31096ca52af
ياسر الأمريكي <70416702+yasiralamriki@users.noreply.github.com>
2025-07-11 09:52:07 -0400

    fix(language-toggle): remove rotation effects from language icons

--- src/components/language-toggle.tsx
+++ src/components/language-toggle.tsx
@@ -13,8 +13,8 @@ export function LanguageToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Languages className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Languages className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <Languages className="h-[1.2rem] w-[1.2rem] scale-100 transition-all dark:scale-0" />
+          <Languages className="absolute h-[1.2rem] w-[1.2rem] scale-0 transition-all dark:scale-100" />
           <span className="sr-only">Toggle language</span>
         </Button>
       </DropdownMenuTrigger>
